53a7f04b7041ecf0556a0f7bf87056f999a81412
pepo <pepo@ukr.net>
2019-07-19 10:27:19 +0300

    1907hw

--- httpServer.js
+++ httpServer.js
@@ -1,45 +1,37 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+
 const server = http.createServer((req, res) => {
     let filePath = path.join(
         __dirname,
         'public',
-        req.url === '/' ? 'index.html':req.url
+        req.url === '/' ? 'index.html' : req.url
     )
     let extname = path.extname(filePath);
     let contentType = 'text/html';
-    if(req.url === '/'){
-        fs.readFile(__dirname + '/public/index.html', 'utf-8', (err, data)=>{
-            res.writeHead(200, {'Contant-Type': 'text/html'});
-            res.end(data);
-        })
-    } else{
-        fs.readFile(__dirname + '/public/' + req.url, 'utf-8', (err, data)=>{
-
-            res.writeHead(200, {'Contant-Type': 'text/html'});
-            res.end(data);
-        })
+    switch (extname) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
     }
-/*  if(req.url === '/'){
-fs.readFile(__dirname + '/public/index.html', 'utf-8', (err, data)=>{
-    res.writeHead(200, {'Contant-Type': 'text/html'});
-    res.end(data);
-})
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+            
+            res.writeHead(404, 'utf-8', { "Content-Type": contentType });
+            res.end('ошибка страницы' );
+        } else {
+            res.writeHead(200, { "Content-Type": contentType })
+            res.end(data)
+        }
+    })
+});
 
-}
-else if(req.url ==='/about'){
-    res.writeHead(400, {'Contant-Type': 'text/plain'} );
-    res.end('Here is text about us')
-}
-else{
-    res.writeHead(400, {'Contant-Type': 'text/plain'} );
-    res.end('<h2> try going to/</h2><h1>404/</h1>')
-}  */
-
-})
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-    console.log('port 3000')
+    console.log('Listening on port 3000')
 })
